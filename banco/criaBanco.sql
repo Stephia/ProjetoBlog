03bdc456636e61e629b267f14e29114814a410dd
Stephia <stephaniefhatida@hotmail.com>
2024-09-12 10:48:36 -0300

    third

--- banco/criaBanco.sql
+++ banco/criaBanco.sql
@@ -2,17 +2,17 @@ DROP DATABASE IF EXISTS BLOG;
 CREATE DATABASE IF NOT EXISTS BLOG;
 
 USE BLOG;
-CREATE TABLE Cliente(
+CREATE TABLE Usuario(
 	id 	      INT AUTO_INCREMENT,
     nome      VARCHAR(50) ,
     email     VARCHAR(50),
     senha     VARCHAR(10),
     ativo     BOOL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id) );
 
-INSERT INTO Cliente VALUES
-	(1, 'Aurora', 'aurorinha@gmail.com', '409094l0v3', false, 4),
-    (2, 'Breno', 'breninhodograu@gmail.com', '1234567890', true, 3),
-    (3, 'Carolaine', 'carolainess@hotmail.com', 'eu231a23nh4', true, 1),
-    (4, 'Dalberto', 'adalberto@yahoo.com.br', '4d4lb37t0', true, 5),
-    (5, 'Evellyn', 'xevellynx@hotmail.com', '^0n;[Q=69w', false, 2)
+INSERT INTO Usuario VALUES
+	(1, 'Aurora', 'aurorinha@gmail.com', '409094l0v3', false),
+    (2, 'Breno', 'breninhodograu@gmail.com', '1234567890', true),
+    (3, 'Carolaine', 'carolainess@hotmail.com', 'eu231a23nh4', true),
+    (4, 'Dalberto', 'adalberto@yahoo.com.br', '4d4lb37t0', true),
+    (5, 'Evellyn', 'xevellynx@hotmail.com', '^0n;[Q=69w', false);
---
+++ login/login2.php
@@ -0,0 +1,20 @@
+<?php
+    include('../includes/conexao.php');
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $sql = "SELECT * FROM Usuario WHERE email = '$email'";
+
+    $result = mysqli_query($con,$sql);
+    if(mysqli_num_rows($result) > 0){
+        $row = mysqli_fetch_array($result);
+        if($row['senha'] == $senha){
+            include('inicia_sessao.php');
+            $_SESSION['login'] = $row;
+            header('Location: index.php');
+        }else{
+            echo "<h1>Senha Inválida!</h1>";
+        }
+    }else{
+        echo "<h1>Usuário não encontrado</h1>";
+    }
