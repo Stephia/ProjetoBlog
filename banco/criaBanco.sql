e8c272ff5c90c28f23cb7b60d85d8488f33e8fdc
Stephia <stephaniefhatida@hotmail.com>
2024-09-19 10:20:28 -0300

    Fifth

--- banco/criaBanco.sql
+++ banco/criaBanco.sql
@@ -3,12 +3,25 @@ CREATE DATABASE IF NOT EXISTS BLOG;
 
 USE BLOG;
 CREATE TABLE Usuario(
-	id 	      INT AUTO_INCREMENT,
-    nome      VARCHAR(50) ,
-    email     VARCHAR(50),
-    senha     VARCHAR(10),
-    ativo     BOOL,
+	id_u 	        INT AUTO_INCREMENT NOT NULL,
+    nome            VARCHAR(50) NOT NULL,
+    email           VARCHAR(255) NOT NULL,
+    senha           VARCHAR(60) NOT NULL,
+    data_criacao    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    ativo           TINYINT(4) NOT NULL DEFAULT '0',
+    adm             TINYINT(4) NOT NULL DEFAULT '0',
     PRIMARY KEY (id) );
+
+CREATE TABLE Post(
+	id_p 	        INT AUTO_INCREMENT NOT NULL,
+    titulo          VARCHAR(255) NOT NULL,
+    texto           TEXT NOT NULL,
+    id_us           INT(11) NOT NULL,
+    data_criacao    DATETIME NOT NULL CURRENT_TIMESTAMP,
+    data_postagem   DATETIME NOT NULL,
+    PRIMARY KEY (id) ),
+    KEY fk_Post_Usuario_idx (id_us),
+    CONSTRAINT fk_Post_Usuario FOREIGN KEY (id_us) REFERENCES Usuario (id_u)
 
 INSERT INTO Usuario VALUES
 	(1, 'Aurora', 'aurorinha@gmail.com', '409094l0v3', false),
