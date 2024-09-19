3f40336561a5f8f401cb83162d33b6a34e8be73f
Stephia <stephaniefhatida@hotmail.com>
2024-09-19 11:37:01 -0300

    sixth

--- core/mysql.php
+++ core/mysql.php
@@ -30,3 +30,50 @@ function insere(string $entidade, array $dados) : bool
 
     return $retorno;
 }
+
+function atualiza(string $entidade, array $dados, array $criterio = []) : bool
+{
+    $retorno = false;
+
+    foreach ($dados as $campo => $dado) {
+        $coringa_dados[$campo] = '?';
+        $tipo[] = gettype($dado) [0];
+        $$campo = $dado;
+    }
+
+    foreach ($criterio as $expressao) {
+        $dado = $expressao[count($expressao) -1];
+
+        $tipo[] = gettype($dado) [0];
+        $expressao[count($expressao) -1] = '?';
+        $coringa_criterio[] = $expressao;
+
+        $nome_campo = (count($expressao) < 4) ? $expressao[0] : $expressao[1];
+
+        if(isset($nome_campo)) {
+            $nome_campo = $nome_campo . '_' . rand();
+        }
+
+        $campos_criterio[] = $nome_campo;
+
+        $$nome_campo = $dado;
+    }
+
+    $instrucao = update($entidade, $coringa_dados, $coringa_criterio);
+
+    $conexao = conecta();
+
+    $stmt = mysqli_prepare($conexao, $instrucao);
+
+    if(isset($tipo)) {
+        $comando = 'mysqli_stmt_bind_param($stmt,';
+        $comando .= "'" . implode('', $tipo). "'";
+        $comando .= ', $' . implode(', $', array_keys($dados));
+        $comando .= ', $' . implode(', $', $campos_criterio);
+        $comando .= ');';
+
+        eval($comando);
+    }
+
+    
+}
