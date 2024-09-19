a31db694a799033f2fe6cf3b2a1fd15c3f91fbbb
Stephia <stephaniefhatida@hotmail.com>
2024-09-19 11:05:45 -0300

    sixth

--- tests/sql_test.php
+++ tests/sql_test.php
@@ -1,2 +1,30 @@
 <?php
     require_once '../core/sql.php';
+
+    $id_u = 1;
+    $nome = 'Murilo';
+    $email = 'murilo@gmail.com';
+    $senha = '123mudar';
+    $dados = ['nome' => $nome, 'email' => $email, 'senha' => $senha];
+
+    $entidade = 'usuario';
+    $criterio = [['id', '=', $id]];
+    $campos = ['id', 'nome', 'email'];
+    print_r($dados);
+    echo '<br>';
+    print_r($campos);
+    echo '<br>';
+    print_r($criterio);
+    echo '<br>';
+    // Teste geração INSERT
+    $instrucao = insert($entidade, $dados);
+    echo $instrucao.'<br>';
+    // Teste geração UPDATE
+    $instrucao = update($entidade, $dados, $criterio);
+    echo $instrucao.'<br>';
+    // Teste geração SELECT
+    $instrucao = select($entidade, $campos, $criterio);
+    // Teste geração DELETE
+    $instrucao = delete($entidade, $criterio);
+    echo $instrucao.'<br>';
+?>
