1557c27d17783cc4d6786dd6ae8a38e49b3d3f7f
Stephia <stephaniefhatida@hotmail.com>
2024-10-03 12:28:49 -0300

    eigth

---
+++ post_detalhe.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<?php
+    require_once 'includes/funcoes.php';
+    require_once 'core/conexao_mysql.php';
+    require_once 'core/sql.php';
+    require_once 'core/mysql.php';
+
+    foreach($_GET as $indice => $dado) {
+        $$indice = limparDados($dado);
+    }
+
+    $posts = buscar(
+        'post',
+        [
+            'titulo',
+        ]
+    )
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
