0a6feea84a8d15bd8ec0b017466b038430c9ffcf
Stephia <stephaniefhatida@hotmail.com>
2024-10-03 11:34:13 -0300

    eigth

--- includes/topo.php
+++ includes/topo.php
@@ -1,3 +1,15 @@
 <?php
     session_start();
 ?>
+
+<div class="card">
+    <div class="card-header">
+        <hl> Projeto Blog em PHP + MySQL IFSP - MURILO</hl>
+    </div>
+    <?php if(isset($_SESSION['login'])): ?>
+    <div class="card-body text-right">
+        Olá <?php echo $_SESSION['login']['usuario']['nome']?>!
+        <a href="core/usuario_repositorio.php?acao=logout" class="btn btn-link btn-sm" role="button">Sair</a>
+    </div>
+    <?php endif ?>
+</div>
